b288945cd5e63eb511d19ee92a48abb8ad18c1b9
Doyeon Kim <nsa10050@naver.com>
2025-04-18 10:56:39 +0900

    vercel 배포 설정 완료

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ const SERVICE_KEY = '356b0d91-82e9-43e0-b690-b78a982ec774';
 
 // 공공 API 데이터 요청
 export const publicData = async ({ numOfRows = 100, pageNo = 1 } = {}) => {
+  console.log(process.env.REACT_APP_API_URL)
   const response = await axios.get(`${process.env.REACT_APP_API_URL}/kcisa`, {
     params: { numOfRows, pageNo },
   });
